07b445544bf525b0385e44afe775846fff6d640d
Никита Фомин <defdXs@yandex.ru>
2024-03-04 20:20:57 +0300

    Info panel

--- Views/MaestroPage.xaml.cs
+++ Views/MaestroPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 using Winestro_A.ViewModels;
 
@@ -6,6 +7,12 @@ namespace Winestro_A.Views;
 
 public sealed partial class MaestroPage : Page
 {
+    public string ConnectionStatus { get; set; }
+    public string GuildsConnected { get; set; }
+    public string PlayersActive { get; set; }
+    public string CurrentTime { get; set; }
+    public string RunTime { get; set; }
+
     public MaestroViewModel ViewModel
     {
         get;
