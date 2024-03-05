fb98837ea03a3c7381165029f55a067ffa5054a3
Никита Фомин <defdXs@yandex.ru>
2024-03-05 16:43:46 +0300

    Bind ObservableCollection with Console Chat History to ListView

---
+++ Services/IntegratedConsoleService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Winestro_A.Services;
+public class IntegratedConsoleService
+{
+    public static ObservableCollection<TextBlock> ConsoleHistory { get; private set; } = new();
+}
