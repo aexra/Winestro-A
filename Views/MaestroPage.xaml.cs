207e466a8d9cca416960ace955afbb475bb55ac7
Никита Фомин <defdXs@yandex.ru>
2024-03-06 00:18:06 +0300

    Godlike formatting (jk)

--- Views/MaestroPage.xaml.cs
+++ Views/MaestroPage.xaml.cs
@@ -8,11 +8,11 @@ namespace Winestro_A.Views;
 
 public sealed partial class MaestroPage : Page
 {
-    public string ConnectionStatus { get; set; }
-    public string GuildsConnected { get; set; }
-    public string PlayersActive { get; set; }
-    public string CurrentTime { get; set; }
-    public string RunTime { get; set; }
+    public string ConnectionStatus  { get; set; }
+    public string GuildsConnected   { get; set; }
+    public string PlayersActive     { get; set; }
+    public string CurrentTime       { get; set; }
+    public string RunTime           { get; set; }
 
     public MaestroViewModel ViewModel
     {
