ba4ea5899de30154dfc3e9684c7a1ed664711dd4
Никита Фомин <defdXs@yandex.ru>
2024-03-07 01:42:58 +0300

    eeeeeeeeeeeeeeeee

--- Views/ConfigPage.xaml.cs
+++ Views/ConfigPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.UI.Xaml.Controls;
-
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Winestro_A.Services;
 using Winestro_A.ViewModels;
 
 namespace Winestro_A.Views;
@@ -24,8 +25,25 @@ public sealed partial class ConfigPage : Page
         
     }
 
-    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private async void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
+        ContentDialog dialog = new ContentDialog();
 
+        // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
+        dialog.XamlRoot = this.XamlRoot;
+        dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+        dialog.Title = "New setting";
+        dialog.PrimaryButtonText = "Create";
+        dialog.CloseButtonText = "Cancel";
+        dialog.DefaultButton = ContentDialogButton.Primary;
+        //dialog.Content = new ContentDialogContent();
+
+        var result = await dialog.ShowAsync();
+
+
+        //if (!ConfigService.AddSetting("", "")) 
+        //{
+            
+        //}
     }
 }
