c42542cbfe44860c2d7147f9c1e3ff576a82931f
Никита Фомин <defdXs@yandex.ru>
2024-03-06 18:40:07 +0300

    Added ICCommandAttribute to register console commands

---
+++ Attributes/ICCommandAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winestro_A.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+class ICCommandAttribute : Attribute
+{
+    public string Name { get; set; }
+    public int nArgs { get; set; } = 0;
+    public string[]? KwargsKeys { get; set; } = null;
+
+    public ICCommandAttribute(string name)
+    {
+        Name = name;
+    }
+}
