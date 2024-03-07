4f07337bce6e74922aea95c56ee437ea6dca842f
Никита Фомин <desktop13254@gmail.com>
2024-03-07 09:38:02 +0300

    Created DiscordBotService service for managing discord bot

---
+++ Services/DiscordBotService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winestro_A.Services;
+
+public class DiscordBotService
+{
+
+
+    public async void Run()
+    {
+
+    }
+}
