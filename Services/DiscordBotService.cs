e004d134e0d0621015c711695cfa32d20d165239
Никита Фомин <desktop13254@gmail.com>
2024-03-07 09:50:49 +0300

    Added discord logs handler

--- Services/DiscordBotService.cs
+++ Services/DiscordBotService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Discord;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,28 @@ public class DiscordBotService
     public static async Task Run()
     {
 
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        switch (msg.Severity)
+        {
+            case LogSeverity.Error:
+                LogService.Error(msg.Message);
+                break;
+            case LogSeverity.Warning:
+                LogService.Warning(msg.Message);
+                break;
+            case LogSeverity.Info:
+                LogService.Log(msg.Message);
+                break;
+            case LogSeverity.Critical:
+                LogService.Error(msg.Message);
+                break;
+            default:
+                LogService.Log(msg.Message, Enums.LogMessageMetaTypes.Debug);
+                break;
+        }
+        return Task.CompletedTask;
     }
 }
