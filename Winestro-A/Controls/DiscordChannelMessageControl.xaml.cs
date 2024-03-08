3b14c319afa6eda8639e49aa8181af98c9f88265
Никита Фомин <defdXs@yandex.ru>
2024-03-09 02:29:41 +0300

    eeeeeeeeee

--- Winestro-A/Controls/DiscordChannelMessageControl.xaml.cs
+++ Winestro-A/Controls/DiscordChannelMessageControl.xaml.cs
@@ -14,6 +14,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Discord;
 using Discord.WebSocket;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -29,8 +30,27 @@ public sealed partial class DiscordChannelMessageControl : UserControl
     public DiscordChannelMessageControl(IMessage msg)
     {
         this.InitializeComponent();
+        FillData(msg);
+    }
+
+    private void FillData(IMessage msg)
+    {
         Message = msg;
         AvatarUrl = msg.Author.GetAvatarUrl();
         DisplayName = ((SocketGuildUser)msg.Author).DisplayName;
+
+        foreach (var att in msg.Attachments)
+        {
+            if (att.ContentType != null)
+            {
+                if (att.ContentType.Contains("image"))
+                {
+                    var img = new Microsoft.UI.Xaml.Controls.Image();
+                    img.Source = new BitmapImage(new Uri(att.Url));
+
+                    ContentStackPanel.Children.Add(img);
+                }
+            }
+        }
     }
 }
