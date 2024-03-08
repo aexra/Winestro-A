53185c126ae7d48e4b658387021223d667c48895
Никита Фомин <defdXs@yandex.ru>
2024-03-09 00:34:07 +0300

    seems like good

--- Winestro-A/Views/GuildsPage.xaml.cs
+++ Winestro-A/Views/GuildsPage.xaml.cs
@@ -4,13 +4,15 @@ using Discord.Rest;
 using Microsoft.UI.Xaml.Controls;
 using Winestro_A.Controls;
 using Winestro_A.Discord;
+using Winestro_A.Services;
 using Winestro_A.ViewModels;
 
 namespace Winestro_A.Views;
 
 public sealed partial class GuildsPage : Page
 {
-    public ulong SelectedGuildId { get; set; }
+    public ulong? SelectedGuildId { get; set; }
+    public ulong? SelectedChannelId { get; set; }
 
     public ObservableCollection<GuildButton> GuildsButtons { get; set; } = new();
     public ObservableCollection<ChannelButton> ChannelsButtons { get; set; } = new();
@@ -90,17 +92,49 @@ public sealed partial class GuildsPage : Page
     private async void ChannelsLV_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         var control = (ChannelButton)ChannelsLV.SelectedItem;
-        if (control == null) return;
+        if (control == null)
+        {
+            SelectedChannelId = null;
+            await LoadSelectedChannelIfNotNull();
+            return;
+        }
 
         var channel = await DiscordBotService.GetTextChannelAsync(control.ChannelId);
 
         if (channel != null) 
         {
+            SelectedChannelId = channel.Id;
             ChannelNameTB.Text = channel.Name;
         }
         else
         {
+            SelectedChannelId = null;
             ChannelNameTB.Text = "[ERROR GETTING CHANNEL NAME]";
+        }
+
+        await LoadSelectedChannelIfNotNull();
+    }
+    private void EmptyChannelHistory()
+    {
+        MessagesControls.Clear();
+    }
+    private async Task FillChannelHistory(ulong channel)
+    {
+        
+    }
+    private async Task LoadSelectedChannelIfNotNull()
+    {
+        MessageBox.Text = string.Empty;
+        if (SelectedChannelId == null)
+        {
+            EmptyChannelHistory();
+            MessageBox.IsReadOnly = true;
+            return;
+        }
+        else
+        {
+            MessageBox.IsReadOnly = false;
+            await FillChannelHistory((ulong)SelectedChannelId);
         }
     }
 }
