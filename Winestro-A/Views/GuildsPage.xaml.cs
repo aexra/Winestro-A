d9d2f0aed39b82aeca3c52f70a58684fb215a786
Никита Фомин <defdXs@yandex.ru>
2024-03-09 18:53:34 +0300

    fixed double messages

--- Winestro-A/Views/GuildsPage.xaml.cs
+++ Winestro-A/Views/GuildsPage.xaml.cs
@@ -200,8 +200,8 @@ public sealed partial class GuildsPage : Page
         var tb = (TextBox)sender;
         if (e.Key == Windows.System.VirtualKey.Enter && !string.IsNullOrWhiteSpace(tb.Text) && SelectedChannelId != null)
         {
-            await DiscordBotService.SendTextMessageAsync((ulong)SelectedChannelId, tb.Text);
             tb.Text = string.Empty;
+            await DiscordBotService.SendTextMessageAsync((ulong)SelectedChannelId, tb.Text);
         }
     }
 }
