e54c3330d79eff5ff14c25bf16fe53ddd9d4a4b6
Никита Фомин <defdXs@yandex.ru>
2024-03-11 00:15:59 +0300

    some slash commands things

--- Winestro-A/Discord/DiscordBotSlashCommands.cs
+++ Winestro-A/Discord/DiscordBotSlashCommands.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Winestro_A.Services;
 
 namespace Winestro_A.Discord;
@@ -17,7 +18,26 @@ public partial class DiscordBotService
     }
     public static async Task RegisterTestSlashCommands()
     {
+        IGuild guild;
+        try
+        {
+            guild = GetGuild(ulong.Parse(ConfigService.Get("DiscordTestGuildID").ToString()));
+        }
+        catch { return; }
 
+        var guildCommand = new SlashCommandBuilder();
+        guildCommand.WithName("winestro-test");
+        guildCommand.WithDescription("This is my first guild slash command!");
+
+        try
+        {
+            await guild.CreateApplicationCommandAsync(guildCommand.Build());
+        }
+        catch 
+        {
+            LogService.Error("Error registering Discord Bot *TEST* slash commands");
+            return; 
+        }
 
         LogService.Log("Discord Bot *TEST* slash commands have been registered");
     }
